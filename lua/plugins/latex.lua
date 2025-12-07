c1c49675d36ea655b8b2bbce610b737ed63f0188
misaka <smileMisaka@outlook.com>
2025-12-07 16:56:57 +0800

    lang: add tex extras and some personal config for autoformatting

---
+++ lua/plugins/latex.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "lervag/vimtex",
+    lazy = false, -- lazy-loading will disable inverse search
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      vim.g.vimtex_view_method = "zathura"
+      -- vim.g.vimtex_format_enabled = 1
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    dependencies = { "mason.nvim" },
+    lazy = true,
+    opts = {
+      formatters_by_ft = {
+        tex = { "tex-fmt" },
+      },
+    },
+  },
+  {
+    "mason-org/mason.nvim",
+    optional = true,
+    opts = { ensure_installed = { "tex-fmt" } },
+  },
+}
